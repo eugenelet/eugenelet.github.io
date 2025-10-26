7beba9d948f83566005e7862acc2120f6e7590b9
eugenelet <eugeneleeeutzuan@gmail.com>
2025-10-26 01:57:06 +0000

    Deploying to gh-pages from @ eugenelet/eugenelet.github.io@fda56e99dcc5d53f17577b8b2adf637d63aa42eb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,35 +501,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%65%75%67%65%6E%65%6C%65%65%65%75%74%7A%75%61%6E@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=RWdloGkAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
